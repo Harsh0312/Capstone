fd253db7974daef14ad318577115b430d282e070
Harsh0312 <singharsh0312@gmail.com>
2022-10-10 16:58:12 +0530

    newly ts created-10-10-2022-4:58

--- AbcHealthCareApi/Startup.cs
+++ AbcHealthCareApi/Startup.cs
@@ -26,6 +26,7 @@ namespace AbcHealthCareApi
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,9 +37,12 @@ namespace AbcHealthCareApi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(opt => { opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000"); 
+            });
 
             app.UseAuthorization();
 
